e3b7418195f47cf0d5a609f352c890efb2a034b5
Renal Apriansyah <renal.apr@gmail.com>
2018-04-18 06:00:03 -0400

    add address book promise task

--- controller.js
+++ controller.js
@@ -6,35 +6,53 @@ const View = require('./view')
 class Controller{
     static saveContact(name,company,telp,email){
         let contact= new Contact(name, company, telp, email)
-         contact.save().then(message=>{
-             View.show(message)
-         })
+        contact.save().then(message=>{
+            View.show(message)
+        })
+        .catch(err => {
+            View.show(err)
+        })
     }
  
     static updateContact(property,value,id){
         let contact = new Contact()
-        contact.update(property, value, id).then(message=>{
+        contact.update(property, value, id)
+        .then(message=>{
             View.show(message)
+        })
+        .catch(err => {
+            View.show(err)
         })
     }   
 
     static deleteContact(deleteId) {
         let contact = new Contact()
-        contact.delete(deleteId).then(message =>{
+        contact.delete(deleteId)
+        .then(message =>{
             View.show(message)
 
         }) 
+        .catch(err => {
+            View.show(err)
+        })
     }
     static saveGroup(name){
         let group = new Group(name)
-        group.save().then(message => {
+        group.save()
+        .then(message => {
             View.show(message)
+        })
+        .catch(err => {
+            View.show(err)
         })
     }
     static updateGroup(groupsValue,groupsId){
         let group = new Group()
         group.update(groupsValue,groupsId).then(message=>{
             View.show(message)
+        })
+        .catch(err => {
+            View.show(err)
         }) 
     }
     static deleteGroup(deleteId) {
@@ -42,11 +60,18 @@ class Controller{
         group.delete(deleteId).then(message=>{
             View.show(message)
         })
+        .catch(err => {
+            View.show(err)
+        })
     }
     static assign(contactId,groupsId){
         let group = new Group()
-        group.assign(contactId, groupsId).then(message=>{
+        group.assign(contactId, groupsId)
+        .then(message=>{
             View.show(message)
+        })
+        .catch(err => {
+            View.show(err)
         })
     }
     static showContact(){
@@ -55,11 +80,18 @@ class Controller{
         .then(message=>{
             View.show(message)
         })
+        .catch(err => {
+            View.show(err)
+        })
     }
     static showGroup(){
         let group = new Group()
-        group.showGroup().then(message => {
+        group.showGroup()
+        .then(message => {
             View.show(message)
+        })
+        .catch(err=>{
+            View.show(err)
         })
     }
     
